59f4ae641cb455acfed327d505d187d7670469cf
sl-yukiokuno <y.okuno@scene-live.com>
2021-11-01 22:12:40 +0900

    add:カレンダーページの実装

--- src/store/calendar-event.model.ts
+++ src/store/calendar-event.model.ts
@@ -1,3 +1,6 @@
+import { reactive } from '@vue/composition-api';
+import { calendarEventMockData } from './calendar-event';
+
 interface CalendarEventDetailBase {
   id: string;
   userId: string;
@@ -14,4 +17,14 @@ export interface CalendarEventDetail extends CalendarEventDetailBase {
 export interface CalendarEventTodayDetail extends CalendarEventDetailBase {
   startTime: string | null;
   endTime: string | null;
+}
+
+export interface NewCalendarEvent {
+  id: string | null;
+  userId: string | null;
+  name: string | null;
+  start: string;
+  end: string | null;
+  memo: string | null;
+  shared: boolean;
 }
